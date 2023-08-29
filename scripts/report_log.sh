2b1fdc2074b048bfb605d82abaf8c65ec255bed1
prog470dev <tshimizudev4@gmail.com>
2023-08-29 03:24:52 +0000

    alp の結果のURIを正規表現で分類

--- scripts/report_log.sh
+++ scripts/report_log.sh
@@ -6,6 +6,7 @@ cd `dirname $0`
 # TODO: -m でルーティングに合わせた正規表現を設定する
 sudo alp json \
   --sort sum  -r \
+  -m "/api/organizer/player/\w+/disqualified,/api/organizer/competition/\w+/finish,/api/organizer/competition/\w+/score,/api/player/player/\w+,/api/player/competition/\w+/ranking" \
   -o count,method,uri,min,avg,max,sum \
   < /var/log/nginx/access.log \
   > ../reports/alp.log
